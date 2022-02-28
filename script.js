735d1bdee005aada49b5f40b2d212a68aa3bc2d1
Monica Björk <m.bjork95@gmail.com>
2022-02-28 14:58:16 +0100

    plantes in position

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const bodiesWrapperElem = document.querySelector('.bodiesWrapper');
+
 const baseUrl = 'https://fathomless-shelf-54969.herokuapp.com/';
 //let key = '';
 
@@ -14,10 +16,10 @@ async function getKey(){
     let data = await response.json();
     console.log('getKey() - Key: ', data.key);
     const key = data.key;
-    getPlanets(key);
+    getBodies(key);
 }
 
-async function getPlanets(key) {
+async function getBodies(key) {
     let response = await fetch(`${baseUrl}bodies`, {
         method: 'GET',
         headers: {'x-zocom': key}
@@ -25,5 +27,35 @@ async function getPlanets(key) {
     console.log("getPlanets() - response: ", response);
 
     let data = await response.json();
-    console.table( data.bodies);
+    console.table(data.bodies);
+    generateBodies(data.bodies);
+}
+
+function generateBodies(bodies) {
+    for(let body of bodies){
+        /* Defining data */
+        const bodyElem = document.createElement('aside');
+        const planetSize = (body.circumference / 2000);
+        const starSize = (body.circumference / 6000);
+        const starHidden = -(starSize/10)*9;
+
+        bodyElem.innerHTML = body.name;
+
+        bodyElem.classList.add('bodiesWrapper--body');
+        bodiesWrapperElem.style.marginLeft = starHidden*-3 + "px";
+        
+        if(body.type == "star") {
+            bodyElem.classList.add('body--star');
+            bodyElem.style.height = starSize + "px";
+            bodyElem.style.width = starSize + "px";
+            bodyElem.style.left = starHidden + "px";
+        } else if(body.type == "planet") {
+            bodyElem.style.height = planetSize + "px";
+            bodyElem.style.width = planetSize + "px";
+        } else {
+            console.log();
+        }
+        
+        bodiesWrapperElem.appendChild(bodyElem);
+    }
 }
