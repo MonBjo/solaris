9788bb9ae7d0e4eb93a7a069bb587d5095c29644
Monica Björk <m.bjork95@gmail.com>
2022-03-03 11:02:26 +0100

    tweaky tweak

--- script.js
+++ script.js
@@ -10,26 +10,7 @@ overlayElem.addEventListener('click', () => {
     overlayElem.style.display = 'none';
 });
 
-async function getKey() {
-    const responseKey = await fetch(`${baseUrl}keys`, {
-        method: 'POST'
-    });
 
-    let dataKey = await responseKey.json();
-    const key = dataKey.key;
-
-    const responseBodies = await fetch(`${baseUrl}bodies`, {
-        method: 'GET',
-        headers: {'x-zocom': key}
-    })
-    
-    let data = await responseBodies.json();
-    console.table(data.bodies);
-    generateBodies(data.bodies);
-    generateEventlisteners(data.bodies);
-}
-
-/*
 async function getKey() {
     const responseKey = await fetch(`${baseUrl}keys`, {
         method: 'POST'
@@ -52,7 +33,6 @@ async function getBodies(key) {
     generateBodies(data.bodies);
     generateEventlisteners(data.bodies);
 }
-*/
 
 function generateEventlisteners(bodies) {
     const bodiesElem = document.querySelectorAll('.bodiesWrapper--body');
